1b5610984ca5decc0339d6a7d80f37637d2b3ad0
Xblivior <1001701@student.sae.edu.au>
2016-03-22 10:45:49 +0800

    PlayerController Update

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour 
+{
+	public float speed;
+	public GameObject thrown;
+	public Transform throwSpawn;
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			Instantiate(thrown, thrown.transform.position, thrown.transform.rotation);
+		}
 	}
 }

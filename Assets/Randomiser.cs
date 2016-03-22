2f75ce7b9252e7e7b7b84376ea3a155c525f4533
Xblivior <1001701@student.sae.edu.au>
2016-03-22 23:20:39 +0800

    Added Randomiser Script

---
+++ Assets/Randomiser.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Randomiser : MonoBehaviour 
+{
+	//Material Arrays
+	public Material[] goalMat;
+	public Material[] playerMat; 
+
+	//Playter and Gaol GameObjects
+	public GameObject goal;
+	public GameObject player;
+
+	//spawn point variables
+	public float xMinG, xMaxG, yMinG, yMaxG; //Gaol
+	public float xMinP, xMaxP, yMinP, yMaxP; //Player
+
+	public GameController gameController;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}

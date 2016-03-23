8199d52caa489c1abaf67dad5091b4a928531e64
Xblivior <1001701@student.sae.edu.au>
2016-03-23 17:09:45 +0800

    Overworld Start
    Imported FirstPersonController Script from standard assets.
    Made ArcadeController Script (is supposed to load throwthething level)
    Made PlayerControllerO script (detects if close to an arcade machine)

---
+++ Assets/ArcadeController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ArcadeController : MonoBehaviour 
+{
+	public string nextLevel;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void NextLevel()
+	{
+		SceneManager.LoadScene (nextLevel);
+	}
+}
---
+++ Assets/PlayerControllerO.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControllerO : MonoBehaviour 
+{
+	GameObject arcade;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		CheckArcade();
+		if (arcade != null)
+		{
+			if (Input.GetKeyDown(KeyCode.F))
+			{
+				arcade.GetComponent<ArcadeController>().NextLevel(); 
+			}
+		}
+	}
+
+	void CheckArcade()
+	{
+		//make a raycast
+		RaycastHit hit;
+		// if the raycast hits something in 2 units
+		if (Physics.Raycast(transform.position, transform.forward, out hit, 2))
+		{
+			
+			//if it hits an aracde machine
+			if (hit.transform.tag == "Arcade")
+			{
+				arcade = this.gameObject;
+			}
+		}
+	}
+}

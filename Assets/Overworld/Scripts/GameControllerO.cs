7cfced043177d49e7734b05bfc273dee7309f612
Xblivior <1001701@student.sae.edu.au>
2016-03-24 15:16:29 +0800

    GameControllerO fixed

--- Assets/Overworld/Scripts/GameControllerO.cs
+++ Assets/Overworld/Scripts/GameControllerO.cs
@@ -21,27 +21,27 @@ public class GameControllerO : MonoBehaviour
 
 	void SpawnArcades()
 	{
-		Vector2 randomPos;
+		Vector2 randomPos = new Vector2();
 		flag = false;
 
-//		while (!flag)
-//		{
-//
-//			randomPos += Random.insideUnitCircle * 10;
-//
-//			//pick a spot
-//			Vector3 possibleSpot = new Vector3 (randomPos.x, 1.5f, randomPos.y);
-//
-//			// if spot is good
-//			if (!Physics.CheckSphere(possibleSpot, 3f, arcadeLayer))
-//			{
-//				//set flag = true
-//				flag = true;
-//				//spawn
-//				Instantiate(arcadeMachines[Random.Range(0, arcadeMachines.Length)]);
-//			}
-//
-//		}
+		while (!flag)
+		{
+
+			randomPos += Random.insideUnitCircle * 10;
+
+			//pick a spot
+			Vector3 possibleSpot = new Vector3 (randomPos.x, 1.5f, randomPos.y);
+
+			// if spot is good
+			if (!Physics.CheckSphere(possibleSpot, 3f, arcadeLayer))
+			{
+				//set flag = true
+				flag = true;
+				//spawn
+				Instantiate(arcadeMachines[Random.Range(0, arcadeMachines.Length)]);
+			}
+
+		}
 		
 	}
 }

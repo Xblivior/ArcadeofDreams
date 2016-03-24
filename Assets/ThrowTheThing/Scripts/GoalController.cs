3eba2e3cf0c74271fdd8bd5b232b4eac2890df21
Xblivior <1001701@student.sae.edu.au>
2016-03-24 16:46:19 +0800

    ThrowTheThing is now 2D

--- Assets/ThrowTheThing/Scripts/GoalController.cs
+++ Assets/ThrowTheThing/Scripts/GoalController.cs
@@ -5,11 +5,12 @@ public class GoalController : MonoBehaviour
 {
 	public GameObject gameController;
 
-	void OnTriggerEnter (Collider other)
+	void OnTriggerEnter2D (Collider2D other)
 	{
 		//if the thrown thing hits the goal
 		if (other.gameObject.tag == "Thrown")
 		{
+			
 			// tell the gamecontroller its a win
 			gameController.GetComponent<GameController>().Victory();
 			Destroy(other.gameObject);

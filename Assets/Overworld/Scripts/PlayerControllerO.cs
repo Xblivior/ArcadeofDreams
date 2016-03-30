9032a7c0577fca625c2e927dd68b9b07c273b5df
Xblivior <1001701@student.sae.edu.au>
2016-03-30 13:56:48 +0800

    The ground follows the player

--- Assets/Overworld/Scripts/PlayerControllerO.cs
+++ Assets/Overworld/Scripts/PlayerControllerO.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class PlayerControllerO : MonoBehaviour 
 {
 	public GameObject arcade;
+	public GameObject ground;
 
 	// Use this for initialization
 	void Start () 
@@ -14,11 +15,19 @@ public class PlayerControllerO : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
+		//make sure the ground follow the player
+		ground.transform.position = new Vector3 (transform.position.x, 0f, transform.position.z);
+
+		//call CheckArcade() to see if player is looking at an arcade
 		CheckArcade();
+
+		// if there is an arcade
 		if (arcade != null)
 		{
+			//and the player presses f
 			if (Input.GetKeyDown(KeyCode.F))
 			{
+				//load the next level
 				arcade.GetComponent<ArcadeController>().NextLevel(); 
 			}
 		}
@@ -35,6 +44,7 @@ public class PlayerControllerO : MonoBehaviour
 			//if it hits an aracde machine
 			if (hit.transform.tag == "Arcade")
 			{
+				//make arcade = to that arcade
 				arcade = hit.transform.gameObject;
 			}
 

1cb1601a31c1e688f89b116037c604f157765238
Xblivior <1001701@student.sae.edu.au>
2016-04-28 17:02:19 +0800

    HighscoreSCript
    Jacobs Highscore saving scripts
    Still have to make them save in Highest at the top and delete if more
    than 10 scores and redo hirearchy

---
+++ Assets/HighScoreEntry.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighScoreEntry   
+{
+
+	public string playerName;
+	public int playerScore;
+
+	public HighScoreEntry(string pN, int pS)
+	{
+		playerName = pN;
+		playerScore = pS;
+	}
+
+}
